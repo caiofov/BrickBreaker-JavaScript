5a991554b57b9486b6a7be55a4b7f3f1f03442c5
caiofov <cfoviana@gmail.com>
2021-10-16 17:54:45 -0300

    the game loop

--- Brick-Breaker/src/index.js
+++ Brick-Breaker/src/index.js
@@ -7,7 +7,22 @@ let context = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-context.clearRect(0,0,800,600); //cleans the screen
 
 let paddle = new Paddle(GAME_WIDTH,GAME_HEIGHT);
 paddle.draw(context);
+
+let lastTime = 0
+
+function gameLoop(timestamp){ //writing a function instead a loop makes it more efficient on slower computers
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+    
+    context.clearRect(0,0,800,600); //cleans the screen
+    paddle.update(deltaTime);
+
+    paddle.draw(context);
+
+    requestAnimationFrame(gameLoop); //when the next frame is ready, the browser will call the next animation frame
+}
+
+gameLoop();
--- Brick-Breaker/src/paddle.js
+++ Brick-Breaker/src/paddle.js
@@ -11,6 +11,16 @@ export default class Paddle {
 
     draw(context){
         console.log("X= ", this.position.x, " Y= ", this.position.y, " w= ",this.width," h = ", this.height);
+
+        context.fillStyle = '#0ff'
         context.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+
+    update(deltaTime){
+        if(!deltaTime){ //if there is no deltatime (at the beggining of the game)
+            return
+        }
+        this.position.x +=5 / deltaTime;
     }
 }
