acd5c4c3a667a2016d5aca0f9b96c7ad4de60fb7
caiofov <cfoviana@gmail.com>
2021-10-16 17:44:17 -0300

    fixing display problems

--- Brick-Breaker/src/index.js
+++ Brick-Breaker/src/index.js
@@ -7,9 +7,7 @@ let context = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-// context.clearRect(0,0,800,600); //cleans the screen
+context.clearRect(0,0,800,600); //cleans the screen
 
 let paddle = new Paddle(GAME_WIDTH,GAME_HEIGHT);
 paddle.draw(context);
-
-context.fillRect(10, 60, 150, 30);
